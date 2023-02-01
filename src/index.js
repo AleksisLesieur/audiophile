d6707642eb0fbe14350cad998d1fd7d34d5c0c04
AleksisLesieur <Aleksisjonaslesieur@gmail.com>
2023-02-02 00:45:15 +0200

    entered details for firebase installation

--- src/index.js
+++ src/index.js
@@ -20,3 +20,43 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+/* help document and connect our API with (or to) our game development team */
+
+// to install firebase
+
+// npm install firebase
+
+// Then, initialize Firebase and begin using the SDKs for the products you'd like to use.
+
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA1hEqaRjNdiyR1o9yhRCqKI2xvNIqg-WQ",
+//   authDomain: "audiophile-react-project.firebaseapp.com",
+//   projectId: "audiophile-react-project",
+//   storageBucket: "audiophile-react-project.appspot.com",
+//   messagingSenderId: "77859307593",
+//   appId: "1:77859307593:web:16c35db7b6cf94e4c53f98",
+//   measurementId: "G-NM3L621ZX9"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+// To host your site with Firebase Hosting, you need the Firebase CLI (a command line tool).
+
+// Run the following npm command to install the CLI or update to the latest CLI version.
+
+// npm install -g firebase-tools
+
+// Doesn't work? Take a look at the Firebase CLI reference or change your npm permissions
+
+// https://audiophile-react-project.web.app/
